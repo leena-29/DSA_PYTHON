a9e91f973d85dd959e00852ffdb4288d2d269d06
LEENA <131523797+leena-29@users.noreply.github.com>
2025-07-17 20:04:56 +0530

    Add files via upload

---
+++ linked_list.py
@@ -0,0 +1,36 @@
+# Day 12: Singly Linked List Implementation in Python
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+# Test the LinkedList
+ll = LinkedList()
+ll.append(10)
+ll.append(20)
+ll.append(30)
+
+print("Linked List:")
+ll.display()
